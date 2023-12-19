3256b29661bb6998e4575bfd0294c269036c0081
Shafiqul Alam Khan <118207040+sakhan-1111@users.noreply.github.com>
2023-12-19 17:04:11 -0600

    Create X star pattern.c

---
+++ X star pattern.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int i, j, n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n); 
+
+    for(i=n; i>=1; i--)
+    {
+        for(j=i; j<n; j++)
+        {
+            printf(" ");
+        }
+        for(j=1; j<=((2*i)-1); j++)
+        {
+            if(j==1 || j==((2*i)-1))
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+ 
+    for(i=2; i<=n; i++)
+    {
+        for(j=i; j<n; j++)
+        {
+            printf(" ");
+        }
+        for(j=1; j<=((2*i)-1); j++)
+        {
+            if(j==1 || j==((2*i)-1))
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
